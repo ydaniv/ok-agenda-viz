0606b95461922128ec276aa19bae0d86ab92d464
Yehonatan Daniv <maggotfish@gmail.com>
2012-10-27 15:11:41 +0200

    Fixed tooltips in example

--- js/agenda-tooltips.js
+++ js/agenda-tooltips.js
@@ -1,5 +1,11 @@
 define(['lib/d3.v2'], function () {
-    var d3 = window.d3;
+    var d3 = window.d3,
+        flor = function (val, _flor) {
+            return val < _flor ? _flor : val;
+        },
+        ciel = function (val, _ciel) {
+            return val > _ciel ? _ciel : val;
+        };
 
     function Tooltip (svg) {
         if ( ! (this instanceof Tooltip) ) return new Tooltip(svg);
@@ -15,6 +21,8 @@ define(['lib/d3.v2'], function () {
             .attr('font-family', 'sans-serif')
             .attr('fill', '#000000')
             .attr('font-size', 12);
+        this.canvas_width = svg.attr('width');
+        this.box_stroke_width = +this.tooltip.style('stroke-width').slice(0, -2);
         this.hideTooltip();
     }
 
@@ -29,19 +37,24 @@ define(['lib/d3.v2'], function () {
             this.container.style("visibility", "hidden");
         },
         updatePosition  : function (x, y) {
-            var padding = 10*2;
-            var margin = 10;
-            var text_w = +this.text.style('width').slice(0, -2) | 0;
-            var text_h = +this.text.style('height').slice(0, -2) | 0;
-            var box_width = text_w + padding;
-            var box_height = text_h + padding;
+            var padding = 10,
+                margin = 10,
+                text_w = +this.text.style('width').slice(0, -2) | 0,
+                text_h = +this.text.style('height').slice(0, -2) | 0,
+                box_width = text_w + 2*padding,
+                box_height = text_h + 2*padding,
+                x_box = x - box_width/2,
+                y_box = y - box_height - margin;
+
+            x_box = ciel(flor(x_box, this.box_stroke_width), this.canvas_width - box_width - 2 * this.box_stroke_width);
+            y_box = flor(y_box, this.box_stroke_width);
 
             this.tooltip.attr('width', box_width)
                 .attr('height', box_height)
-                .attr('x', x - box_width/2)
-                .attr('y', y - box_height - margin);
-            this.text.attr('x', x + text_w/2)
-                .attr('y', y - text_h - margin);
+                .attr('x', x_box)
+                .attr('y', y_box);
+            this.text.attr('x', x_box + text_w + padding + this.box_stroke_width)
+                .attr('y', y_box + text_h + padding - this.box_stroke_width);
         }
     };
 
