9a31131bddccb5d7a208b691145cda9debb28e72
Yehonatan Daniv <maggotfish@gmail.com>
2012-11-01 15:51:02 +0200

    Fixed  broken hash setting of embed snippet

--- src/agenda-viz.js
+++ src/agenda-viz.js
@@ -262,10 +262,13 @@ define(['agenda-charts', 'reqwest', 'when'], function (Charts, Reqwest, When) {
                 dispatcher.change_hash(is_all ? '' : 'party_' + party_id);
             });
             dispatcher.on('change_hash', function (hash) {
+                var match = embed_snippet.match(/src="[^#]+(#?.*)"/);
                 window.location.hash = hash ? '#' + hash : '';
-                embed_snippet = embed_snippet.replace(/src="[^#]+#?(.*)"/, function (match, old_hash) {
-                    return old_hash ? match.replace(old_hash, hash) : match + (~ match.indexOf('#') ? hash : '#' + hash);
-                });
+                if ( match && match.length > 1 ) {
+                    embed_snippet = !!match[1] ?
+                                        embed_snippet.replace(match[1], '#' + hash) :
+                                        embed_snippet.replace(match[0], match[0] + '#' + hash);
+                }
                 d3.select('#embed-snippet').property('value', embed_snippet);
             });
             // IE can't set innerHTML of select, need to use the .options.add interface
